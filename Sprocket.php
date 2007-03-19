baa55049ea6dcb280d122b43143c26698b14232e
codechief <codechief@8ce46ea7-a32b-0410-82ae-1deb9c1c5b82>
2007-03-19 15:16:47 +0000

    Fixed license declaration

--- Sprocket.php
+++ Sprocket.php
@@ -11,25 +11,34 @@
 **  \ \ \  / /\ \ \  / / /       by Eddie Bowen, Keith Avery and Jon Canady
 **   \ \ \/ / /\ \ \/ / /        
 **    \ \/ / /__\_\/ / /       
-**     \  / /______\/ /          Email: ebowen at innova-partners dot com
-**      \/___________/           Copyright (c) 2007, Innova Partners
+**     \  / /______\/ /          
+**      \/___________/           Email: ebowen at innova-partners dot com
 **
 **------------------------------------------------------------------------------
-** The source code included in this package is free software; you can
-** redistribute it and/or modify it under the terms of the GNU General Public
-** License as published by the Free Software Foundation. This license can be
-** read at:
-**
-** http://www.opensource.org/licenses/gpl-license.php
-**
-** This program is distributed in the hope that it will be useful, but WITHOUT 
-** ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
-** FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. 
+** Copyright (c) 2007, Innova Partners
+** Permission is hereby granted, free of charge, to any person obtaining 
+** a copy of this software and associated documentation files (the 
+** "Software"), to deal in the Software without restriction, including 
+** without limitation the rights to use, copy, modify, merge, publish, 
+** distribute, sublicense, and/or sell copies of the Software, and to 
+** permit persons to whom the Software is furnished to do so, subject to 
+** the following conditions:
+
+** The above copyright notice and this permission notice shall be included 
+** in all copies or substantial portions of the Software.
+
+** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+** OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+** MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+** IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY 
+** CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
+** TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+** SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 **------------------------------------------------------------------------------
 */
-
-	require_once 'TextSprocket.php';
 	
+	require_once 'TextSprocket.php';
+
 	/**
 	 * This is the main class you interact with while using phpSprockets.
 	 * 
--- Sprocket_Forms.php
+++ Sprocket_Forms.php
@@ -11,22 +11,33 @@
 **  \ \ \  / /\ \ \  / / /       by Eddie Bowen, Keith Avery and Jon Canady
 **   \ \ \/ / /\ \ \/ / /        
 **    \ \/ / /__\_\/ / /       
-**     \  / /______\/ /          Email: ebowen at innova-partners dot com
-**      \/___________/           Copyright (c) 2007, Innova Partners
+**     \  / /______\/ /          
+**      \/___________/           Email: ebowen at innova-partners dot com
 **
 **------------------------------------------------------------------------------
-** The source code included in this package is free software; you can
-** redistribute it and/or modify it under the terms of the GNU General Public
-** License as published by the Free Software Foundation. This license can be
-** read at:
-**
-** http://www.opensource.org/licenses/gpl-license.php
-**
-** This program is distributed in the hope that it will be useful, but WITHOUT 
-** ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
-** FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. 
+** Copyright (c) 2007, Innova Partners
+** Permission is hereby granted, free of charge, to any person obtaining 
+** a copy of this software and associated documentation files (the 
+** "Software"), to deal in the Software without restriction, including 
+** without limitation the rights to use, copy, modify, merge, publish, 
+** distribute, sublicense, and/or sell copies of the Software, and to 
+** permit persons to whom the Software is furnished to do so, subject to 
+** the following conditions:
+
+** The above copyright notice and this permission notice shall be included 
+** in all copies or substantial portions of the Software.
+
+** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+** OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+** MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+** IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY 
+** CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
+** TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+** SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 **------------------------------------------------------------------------------
 */
+	
+	require_once 'Sprocket.php';
 	
 	/**
 	 * This is the first Sprocket Factory written, so it's sort of the 
@@ -35,9 +46,8 @@
 	 * 
 	 * This class has convenience functions to create [X]HTML Forms.  Rather
 	 * than constructing the Sprockets by hand, or worse yet writing HTML by
-	 * hand with string concatenation, this class simply provides methods that
-	 * string the appropriate Sprockets together and sends you the final parent
-	 * Sprocket.
+	 * hand with string concatenation, this class provides methods that
+	 * assemble the appropriate Sprockets and returns a composite Sprocket.
 	 * 
 	 * @package phpSprockets
 	 */
